951415895ef4931881645b942b259a9320d847e5
muhammednafih09 <muhammednafih09@gmail.com>
2022-08-27 16:34:29 +0530

    removed foot and head

--- components/foot.php
+++
@@ -1,8 +0,0 @@
-<?php function putFoot(SimpleDB $db = null, array $scripts = null) { ?>
-    <footer>
-        This is the footer
-    </footer>
-    <?php foreach ($scripts as $script) : ?>
-        <script src="<?= $script ?>"></script>
-    <?php endforeach; ?>
-<?php } ?>
